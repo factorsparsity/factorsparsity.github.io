fae240dcbc40bbb269be9b3872e913c05f473cf5
factorsparsity <77400796+factorsparsity@users.noreply.github.com>
2025-08-10 20:06:31 -0500

    Update sw.js

--- md/sw.js
+++ md/sw.js
@@ -7,6 +7,7 @@ const APP_SHELL = [
   './icon-512.png',
   './chicago.woff2',
   './courier-prime.woff2',
+  './showdown.min.js',
   'https://cdn.jsdelivr.net/npm/showdown@2/dist/showdown.min.js',
   'https://cdn.jsdelivr.net/npm/idb-keyval@6/dist/idb-keyval.iife.min.js'
 ];
